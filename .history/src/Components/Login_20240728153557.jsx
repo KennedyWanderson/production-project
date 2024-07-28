62591cfe0e03c03494850898aa2de8861453a1fc
KennedyWanderson <kennedy.wanderson410@gmail.com>
2024-07-28 15:39:54 +0100

    update login page

---
+++ .history/src/Components/Login_20240728153557.jsx
@@ -0,0 +1,10 @@
+
+export default function Login(){
+    return(
+        <form onSubmit={handleLogin}>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    )
+}
